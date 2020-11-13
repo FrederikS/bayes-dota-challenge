af3d1da184f52d9cb4308017cc85b2bd72ae1a2b
frederik.steffen <frederik.steffen@gmail.com>
2020-11-13 15:39:23 +0100

    added mapstruct

--- src/main/java/gg/bayes/challenge/service/impl/MatchServiceImpl.java
+++ src/main/java/gg/bayes/challenge/service/impl/MatchServiceImpl.java
@@ -3,6 +3,10 @@ package gg.bayes.challenge.service.impl;
 import gg.bayes.challenge.parser.MatchLogParser;
 import gg.bayes.challenge.persistence.Match;
 import gg.bayes.challenge.persistence.MatchRepository;
+import gg.bayes.challenge.persistence.MatchRepository.DamageStats;
+import gg.bayes.challenge.persistence.MatchRepository.KillStats;
+import gg.bayes.challenge.persistence.MatchRepository.SpellStats;
+import gg.bayes.challenge.persistence.event.HeroEvent.ItemPurchased;
 import gg.bayes.challenge.persistence.event.MatchEventRepository.HeroEventRepository.ItemPurchasedEventRepository;
 import gg.bayes.challenge.rest.model.HeroDamage;
 import gg.bayes.challenge.rest.model.HeroItems;
@@ -11,6 +15,9 @@ import gg.bayes.challenge.rest.model.HeroSpells;
 import gg.bayes.challenge.service.MatchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,6 +31,7 @@ public class MatchServiceImpl implements MatchService {
     private final MatchLogParser matchLogParser;
     private final MatchRepository matchRepository;
     private final ItemPurchasedEventRepository itemPurchasedEventRepository;
+    private final StatsMapper statsMapper;
 
     @Override
     public Long ingestMatch(String payload) {
@@ -36,24 +44,15 @@ public class MatchServiceImpl implements MatchService {
     public List<HeroKills> stats(Long matchId) {
         return matchRepository.findKillStatsByMatchId(matchId)
                               .stream()
-                              .map(s -> {
-                                  final HeroKills heroKills = new HeroKills();
-                                  heroKills.setHero(s.getHero());
-                                  heroKills.setKills(s.getKills());
-                                  return heroKills;
-                              }).collect(Collectors.toList());
+                              .map(statsMapper::toHeroKillsDto)
+                              .collect(Collectors.toList());
     }
 
     @Override
     public List<HeroItems> items(Long matchId, String hero) {
         return itemPurchasedEventRepository.findByMatchIdAndHero(matchId, hero)
                                            .stream()
-                                           .map(e -> {
-                                               final HeroItems heroItems = new HeroItems();
-                                               heroItems.setItem(e.getItem());
-                                               heroItems.setTimestamp(e.getTimestamp());
-                                               return heroItems;
-                                           })
+                                           .map(statsMapper::toHeroItemsDto)
                                            .collect(Collectors.toList());
     }
 
@@ -61,27 +60,29 @@ public class MatchServiceImpl implements MatchService {
     public List<HeroSpells> spells(Long matchId, String hero) {
         return matchRepository.findSpellStatsByMatchIdAndHero(matchId, hero)
                               .stream()
-                              .map(s -> {
-                                  final HeroSpells heroSpells = new HeroSpells();
-                                  heroSpells.setSpell(s.getSpell());
-                                  heroSpells.setCasts(s.getCasts());
-                                  return heroSpells;
-                              }).collect(Collectors.toList());
+                              .map(statsMapper::toHeroSpellsDto)
+                              .collect(Collectors.toList());
     }
 
     @Override
     public List<HeroDamage> damage(Long matchId, String hero) {
         return matchRepository.findDamageStatsByMatchIdAndHero(matchId, hero)
                               .stream()
-                              .map(s -> {
-                                  final HeroDamage heroDamage = new HeroDamage();
-                                  heroDamage.setTarget(s.getTarget());
-                                  heroDamage.setTotalDamage(s.getDamage());
-                                  heroDamage.setDamageInstances(s.getInstances());
-                                  return heroDamage;
-                              }).collect(Collectors.toList());
+                              .map(statsMapper::toHeroDamageDto)
+                              .collect(Collectors.toList());
     }
 
-    // TODO mapstruct
+    @Mapper(componentModel = "spring")
+    interface StatsMapper {
+        HeroKills toHeroKillsDto(KillStats killStats);
+        HeroItems toHeroItemsDto(ItemPurchased itemPurchasedEvent);
+        HeroSpells toHeroSpellsDto(SpellStats spellStats);
+
+        @Mappings({
+                @Mapping(source = "damage", target = "totalDamage"),
+                @Mapping(source = "instances", target = "damageInstances")
+        })
+        HeroDamage toHeroDamageDto(DamageStats damageStats);
+    }
 
 }
